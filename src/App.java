c01198dedd7f509d62bd7591b05cf5ddc345a1ac
viniciusgte <viniciusgte@gmail.com>
2022-10-01 19:40:13 -0300

    100%

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+
+        double raio;
+        double area;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Escreva abaixo o valor do raio: ");
+        raio = teclado.nextDouble();
+
+        area = Math.PI * (Math.pow(raio,2));
+
+        System.out.print("Area do Circulo é: ");
+        System.out.print(area);
+
+
+        teclado.close();
+
+    }
+
+}
